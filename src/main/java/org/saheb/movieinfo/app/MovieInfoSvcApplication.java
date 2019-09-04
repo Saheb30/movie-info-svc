20219a730821fab4dda110a12482634f03ad42ca
Jana <saheb.jana@accenture.com>
2019-09-04 16:34:52 +0530

    Registered as Eureka client

--- src/main/java/org/saheb/movieinfo/app/MovieInfoSvcApplication.java
+++ src/main/java/org/saheb/movieinfo/app/MovieInfoSvcApplication.java
@@ -2,10 +2,12 @@ package org.saheb.movieinfo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages="org.saheb.movieinfo")
+@EnableEurekaClient//Optional
 public class MovieInfoSvcApplication {
 
 	public static void main(String[] args) {
